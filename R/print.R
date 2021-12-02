7d2b235a7c40f790704643fdc29c80d622c77efc
Robin Hankin <hankin.robin@gmail.com>
2021-12-03 11:41:57 +1300

    fixes #2

--- R/print.R
+++ R/print.R
@@ -4,6 +4,7 @@
     if(!is.null(jj)){  
         options("sprayvars" = c(jj,paste("d",jj,sep="")))
     }
+    class(x) <- setdiff(class(x),"weyl")
     if(isTRUE(getOption("polyform",default=FALSE))){
         out <- print_spray_polyform(x)
     } else {
