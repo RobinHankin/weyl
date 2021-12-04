0bd98d62e7eb7c3e201df252bf55cb3ab5862b5d
Robin Hankin <hankin.robin@gmail.com>
2021-12-04 19:26:58 +1300

    slight modification to print method

--- R/print.R
+++ R/print.R
@@ -1,8 +1,18 @@
 `print.weyl` <- function(x,...){
     cat("A member of the Weyl algebra:\n")
     jj <- getOption("weylvars")   # typically c("x","y","z")
-    if(!is.null(jj)){  
+    if(!is.null(jj)){ 
         options("sprayvars" = c(jj,paste("d",jj,sep="")))
+    } else {  # weylvars is NULL -> unset
+        n <- arity(x)/2
+        if(n <= 3){
+            jj <- letters[seq(from=24,len=n)]        
+            jj <- c(jj,paste("d",jj,sep=""))
+        } else {
+            jj <- as.character(seq(from=1,len=n))
+            jj <- c(paste("x",jj,sep=""),paste("d",jj,sep=""))
+        }
+        options("sprayvars" = jj)
     }
     class(x) <- setdiff(class(x),"weyl")
     if(isTRUE(getOption("polyform",default=FALSE))){
