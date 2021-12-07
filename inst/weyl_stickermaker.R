4fd922eb27d8de500d1a9a6784dc855f8e5a908d
Robin Hankin <hankin.robin@gmail.com>
2021-12-07 16:41:38 +1300

    sticker

---
+++ inst/weyl_stickermaker.R
@@ -0,0 +1,5 @@
+library("hexSticker")
+
+sticker("weyl_writing.png", package="weyl", p_size=36, s_x=1, s_y=1.1,
+s_width=1.2, asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
+h_color="#000000", filename="weyl.png")
